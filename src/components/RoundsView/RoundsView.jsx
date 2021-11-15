3bc5a96f1215a0e69c94fd8bfc7a9219a5720382
Matt Dow <matt.dow.vo@gmail.com>
2021-11-15 12:01:00 -0600

    Initialized project

---
+++ src/components/RoundsView/RoundsView.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useEffect, useSelector} from 'react-redux';
+
+function RoundsView() {
+    // get the user's info from the store
+    const user = useSelector((store) => store.user);
+    return (
+        
+    )
+
+
+
+}
+
+
+
+
+
+export default RoundsView;
