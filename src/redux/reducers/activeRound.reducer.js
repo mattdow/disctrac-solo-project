0670fce37ba7f6a4eb62ed9cf174a0b832428545
Matt Dow <matt.dow.vo@gmail.com>
2021-11-19 10:41:06 -0600

    Active round reducer/saga/router refactored

--- src/redux/reducers/activeRound.reducer.js
+++ src/redux/reducers/activeRound.reducer.js
@@ -6,18 +6,30 @@ const emptyNewRound = {
     course_id: null,
     round_id: null,
 }
+// const activeRoundReducer = (state = emptyNewRound, action) => {
+//     switch (action.type) {
+//         case 'START_NEW_ROUND':
+//             // spreading the state object and changing the non-array values
+//             return { ...state, user_id: action.payload.user, course_id: action.payload.course};
+//         case 'SET_ROUND_ID':
+//             // spreading the state object and changing the round_id to the new ID
+//             return { ...state, round_id: action.payload};
+//         case 'LOGOUT':
+//             return {};
+//         default:
+//             return state;
+//     }
+// } // end of currentCourseReducer
+
 const activeRoundReducer = (state = emptyNewRound, action) => {
     switch (action.type) {
-        case 'START_NEW_ROUND':
-            // spreading the state object and changing the non-array values
-            return { ...state, user_id: action.payload.user, course_id: action.payload.course};
-        case 'SET_ROUND_ID':
-            // spreading the state object and changing the round_id to the new ID
-            return { ...state, round_id: action.payload};
+        case 'SET_ACTIVE_ROUND':
+            return { ...state, user_id: action.payload.user_id, course_id: action.payload.course_id, round_id: action.payload.id};
         case 'LOGOUT':
             return {};
         default:
             return state;
     }
-} // end of currentCourseReducer
+} // end of activeRoundReducer
+
 export default activeRoundReducer;
--- src/redux/sagas/rounds.saga.js
+++ src/redux/sagas/rounds.saga.js
@@ -21,6 +21,7 @@ function* postNewRound(action) {
         yield put({type: 'SET_ROUND_ID', payload: response.data.round_id});
         // may need to fetch the new rounds list
         yield put ({type: 'FETCH_ROUNDS'});
+        yield put ({type: 'FETCH_ACTIVE_ROUND', payload: response.data.round_id});
     } catch(err) {
         yield put({ type: 'START_NEW_ROUND_ERROR'});
         console.log('Error in postNewRound', err);
@@ -40,11 +41,23 @@ function* deleteRound(action) {
     }
 }
 
+// define a function to fetch the currently active round from the DB 
+function* fetchActiveRound(action) {
+    try {
+        const response = yield axios.get(`api/rounds/${action.payload}`);
+        yield put({ type: 'SET_ACTIVE_ROUND', payload: response.data})
+    } catch(err) {
+        yield put ({ type: 'FETCH_ACTIVE_ROUND_ERROR'});
+        console.log(err);
+    }
+}
+
 function* roundsSaga() {
     // watching for actions related to the user's rounds
     yield takeLatest('FETCH_ROUNDS', fetchRounds);
     yield takeLatest('START_NEW_ROUND', postNewRound);
     yield takeLatest('DELETE_ROUND', deleteRound);
+    yield takeLatest('FETCH_ACTIVE_ROUND', fetchActiveRound);
 }
 
 export default roundsSaga;
