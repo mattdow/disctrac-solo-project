e70cde879335da0d4cf966efb6eab1b60ccc0f64
Matt Dow <matt.dow.vo@gmail.com>
2021-11-27 12:40:14 -0600

    Changed color, centered title

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import { AppBar, Toolbar, CssBaseline, Typography} from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Link } from 'react-router-dom';
+import './Banner.css';
 
 // Create some styles for use in the component
 
 
-const useStyles = makeStyles((theme) => ({
-    logo: {
-        flexGrow: "1",
-        cursor: "pointer",
-    },
-}));
+// const useStyles = makeStyles((theme) => ({
+//     logo: {
+//         flexGrow: "1",
+//         cursor: "pointer",
+//     },
+// }));
 
 function Banner() {
     // call useStyles to load the classes defined above the function
-    const classes = useStyles();
+    // const classes = useStyles();
     //JSX rendering code
     return (
-        <AppBar position="static">
+        <AppBar position="static" style={{background: '#386641'}} >
             <CssBaseline />
-            <Toolbar>
-                <Typography variant="h4" className={classes.logo}>
+            <Toolbar style={{justifyContent: 'center'}}>
+                <Typography variant="h3">
                     DiscTrac
                 </Typography>
             </Toolbar>
